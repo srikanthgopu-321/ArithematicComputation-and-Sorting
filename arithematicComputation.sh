febc9cfd0129b022b428d21b856954b0a6063dde
srikanthgopu-321 <gopusrikanthreddy71@gmail.com>
2020-10-22 15:19:33 +0530

    added:user input

--- arithematicComputation.sh
+++ arithematicComputation.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 echo "welcome to Arithematic Computation and Sorting"
+echo
+#uc1:taken three user input
+read -p "Enter the a input" input1
+read -p "Enter the b input" input2
+read -p "Enter the c input" input3
