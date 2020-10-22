1116fb21dc50a24145c0cfa2d028bb089a840304
srikanthgopu-321 <gopusrikanthreddy71@gmail.com>
2020-10-22 15:27:44 +0530

    added:compute(a+b*c)

--- arithematicComputation.sh
+++ arithematicComputation.sh
@@ -5,3 +5,7 @@ echo
 read -p "Enter the a input" input1
 read -p "Enter the b input" input2
 read -p "Enter the c input" input3
+echo
+#uc2
+firstEquationResult=$(( $input1 + $input2 * $input3))
+        echo $firstEquationResult
