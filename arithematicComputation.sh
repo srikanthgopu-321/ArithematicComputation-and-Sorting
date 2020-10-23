f6c13af51e1ece309dcf1a514c11a5cda1326526
srikanthgopu-321 <gopusrikanthreddy71@gmail.com>
2020-10-23 14:15:35 +0530

    added:read-value-from-dictionary-into-array

--- arithematicComputation.sh
+++ arithematicComputation.sh
@@ -29,3 +29,90 @@ result[secondEquationResult]=$secondEquationResult
 result[thirdEquationResult]=$thirdEquationResult
 result[forthEquationResult]=$fourthEquationResult
 echo ${result[@]}
+echo "-----------------------------------------------"
+#uc7
+declare -A storeResult
+declare -a arrayElements
+storeResult=(["1"]=0 ["2"]=0 ["3"]=0 ["4"]=0 )
+
+function getInputs()
+{
+read -p "Enter First Number " numberOne
+read -p "Enter Second Number " numberTwo
+read -p "Enter Third Number " numberThree
+
+findResult
+}
+
+function findResult()
+{
+        firstEquationResult=$(( $numberOne + $numberTwo * $numberThree ))
+        storeResult[1]=$firstEquationResult
+
+        secondEquationResult=$(( $numberOne * $numberTwo + $numberThree  ))
+        storeResult[2]=$secondEquationResult
+
+        thirdEquationResult=$(( $numberThree + $numberOne / $numberTwo ))
+        storeResult[3]=$thirdEquationResult
+
+        fourthEquationResult=$(( $numberOne % $numberTwo + $numberThree  ))
+        storeResult[4]=$fourthEquationResult
+}
+
+function readValuesToArray()
+{
+        for ((index=0; index<=${#storeResult[@]}; index++ ))
+        do
+                arrayElements[index]=${storeResult[$index]}
+        done
+        descendingSort
+}
+
+function descendingSort()
+{
+        temp=0
+        for ((i=0; i<${#arrayElements[@]}; i++ ))
+        do
+                for ((j=i+1; j<${#arrayElements[@]}; j++ ))
+                do
+                        if [[ ${arrayElements[i]%.*} -lt ${arrayElements[j]%.*} ]]
+                        then
+                                temp=${arrayElements[i]}
+                                arrayElements[i]=${arrayElements[j]}
+                                arrayElements[j]=$temp
+                        fi
+                done
+        done
+        echo Array In Descending Order..${arrayElements[@]}
+}
+
+getInputs
+readValuesToArray
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
